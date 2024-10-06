b869f986c546445d442f9a25487d0193f3ac8c02
Matteo <125307998+teo-piro-devoloper@users.noreply.github.com>
2024-10-06 15:56:06 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ def main():
      server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
      server_socket.accept() # wait for client
 
+     client, addr = server_socket.accept()  # wait for client
+     client.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
